a3dfa68815c50b56c47b8a78debd72efab4b06a0
felrobotics <lposadaa@eafit.edu.co>
2023-10-26 18:43:09 -0500

    moved folders

--- app/core/settings/base.py
+++ app/core/settings/base.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # External Packages
+    "rest_framework"
+    # Internal Apps
 ]
 
 MIDDLEWARE = [
@@ -111,3 +114,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# DRF
+REST_FRAMEWORK = {}
